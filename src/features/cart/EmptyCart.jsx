1d2e62db51b39c1ba9a83b3a4380242a339f7007
shakibtaheri <shakibtaheri7@gmail.com>
2026-01-04 13:26:00 +0330

    folder structure and pre ready files

---
+++ src/features/cart/EmptyCart.jsx
@@ -0,0 +1,11 @@
+function EmptyCart() {
+  return (
+    <div>
+      <Link to="/menu">&larr; Back to menu</Link>
+
+      <p>Your cart is still empty. Start adding some pizzas :)</p>
+    </div>
+  );
+}
+
+export default EmptyCart;
